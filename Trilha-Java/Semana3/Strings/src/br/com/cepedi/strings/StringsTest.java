087e7f95190109acc32b79fc61863565f61cd7df
joao ramos <jvitorsb98@outlook.com>
2024-01-25 21:04:57 -0300

    Exercitando StringBuider

---
+++ Trilha-Java/Semana3/Strings/src/br/com/cepedi/strings/StringsTest.java
@@ -0,0 +1,12 @@
+package br.com.cepedi.strings;
+
+public class StringsTest {
+	
+	public static void main(String[] args) {
+		String nome = "Joao";
+		String nome2= "Joao";
+		
+		System.out.println(nome==nome2);
+	}
+
+}
