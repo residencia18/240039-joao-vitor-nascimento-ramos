3bd4c46f84365bc1a36efdb37fbf8433f4cd2a3e
joao ramos <jvitorsb98@outlook.com>
2024-01-30 23:16:22 -0300

    Estudando equals

---
+++ Trilha-Java/Semana5/Treinando/CollectionsEquals/src/br/com/cepedi/dominio/Smartphone.java
@@ -0,0 +1,32 @@
+package br.com.cepedi.dominio;
+
+public class Smartphone {
+	private String serialNumber;
+	private String marca;
+	public Smartphone(String serialNumber, String marca) {
+		super();
+		this.serialNumber = serialNumber;
+		this.marca = marca;
+	}
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj==null) return false;
+		if(this==obj) return true;
+		if(this.getClass() != obj.getClass()) return false;
+		Smartphone smart = (Smartphone) obj; 
+		return serialNumber!=null && serialNumber.equals(smart.getSerialNumber());
+	} 
+}
---
+++ Trilha-Java/Semana5/Treinando/CollectionsEquals/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module CollectionsEquals {
+}
