61d6ad94a866663955d4f87c96db41c4232cb14e
joao ramos <jvitorsb98@outlook.com>
2024-02-20 23:53:47 -0300

    curso javascript concluido LocalStorage

--- FRONT-END/Semana4/Praticando/LocalStorage/fokus-local-storage-main/script-crud.js
+++ FRONT-END/Semana4/Praticando/LocalStorage/fokus-local-storage-main/script-crud.js
@@ -1,15 +1,89 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const textArea = document.querySelector('.app__form-textarea');
+const ulTarefas = document.querySelector(".app__section-task-list")
+const btnCancelar = document.querySelector(".app__form-footer__button--cancel")
+const paragrafoDescricaoTarefa = document.querySelector(".app__section-active-task-description")
+const btnRemoverConcluidas = document.querySelector("#btn-remover-concluidas");
+const btbRemoverTodas = document.querySelector("#btn-remover-todas");
 
-const tarefas = [];
-
-function criarElementoTarefa(tarefa){
-    const li = document.createElement('li');
-    li.classList.add('add__section-task-list-item');
-
-    const svg
+let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+let tarefaSelecionada =null;
+let liTarefaSelecionada = null;
+function atualizarTarefas(){
+    localStorage.setItem('tarefas',JSON.stringify(tarefas))
 }
+
+btnCancelar.addEventListener('click',(evt)=>{
+    textArea.value="";
+    formAdicionarTarefa.classList.add('hidden');
+})
+
+function criarElementoTarefa(tarefa) {
+    const li = document.createElement('li')
+    li.classList.add('app__section-task-list-item')
+
+    const svg = document.createElement('svg')
+    svg.innerHTML = `
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+                fill="#01080E"></path>
+        </svg>
+    `
+    const paragrafo = document.createElement('p')
+    paragrafo.textContent = tarefa.descricao;
+    paragrafo.classList.add('app__section-task-list-item-description')
+
+    const botao = document.createElement('button')
+    botao.classList.add('app_button-edit')
+    
+    botao.onclick = () => {
+        const novaDescricao = prompt("Qual é o novo nome da tarefa?")
+        if(novaDescricao){
+            paragrafo.textContent = novaDescricao;
+            tarefa.descricao = novaDescricao;
+            atualizarTarefas();
+        }
+    }
+
+    const imagemBotao = document.createElement('img')
+    imagemBotao.setAttribute('src', './imagens/edit.png')
+    botao.append(imagemBotao)
+
+    li.append(svg)
+    li.append(paragrafo)
+    li.append(botao)
+
+    if(tarefa.completa){
+        li.classList.add("app__section-task-list-item-complete");
+        botao.setAttribute('disabled','disabled');
+    }else{
+        
+        li.onclick = () =>{
+    
+            if(tarefa==tarefaSelecionada){
+                li.classList.remove("app__section-task-list-item-active");
+                paragrafoDescricaoTarefa.textContent="";
+                tarefaSelecionada=null;
+                liTarefaSelecionada=null;
+                return;
+            }
+    
+            paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+            const todosLi = document.querySelectorAll(".app__section-task-list-item-active").forEach( elemento => {
+                elemento.classList.remove("app__section-task-list-item-active");
+            })
+            li.classList.add('app__section-task-list-item-active')
+            tarefaSelecionada = tarefa;
+            liTarefaSelecionada= li;
+        }
+    }
+
+
+    return li
+}   
 
 btnAdicionarTarefa.addEventListener('click',(evt)=>{
     formAdicionarTarefa.classList.toggle('hidden');
@@ -18,8 +92,40 @@ btnAdicionarTarefa.addEventListener('click',(evt)=>{
 formAdicionarTarefa.addEventListener('submit',(evt)=>{
     evt.preventDefault();
     const tarefa ={
-        descricaoTarefa: textArea.value
+        descricao: textArea.value
     }
     tarefas.push(tarefa);
-    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
+    atualizarTarefas()
+    textArea.value = "";
+    formAdicionarTarefa.classList.add('hidden')
 })
+
+tarefas.forEach(tarefa => {
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    ulTarefas.append(elementoTarefa)
+});
+
+
+document.addEventListener('FocoFinalizado', (evt)=>{
+    if(tarefaSelecionada && liTarefaSelecionada){
+        liTarefaSelecionada.classList.remove("app__section-task-list-item-active");
+        liTarefaSelecionada.classList.add("app__section-task-list-item-complete");
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled','disabled');
+        tarefaSelecionada.completa = true;
+        atualizarTarefas()
+    }
+})
+
+const removerTarefas = (somenteCompletas )=>{
+    const seletor = somenteCompletas ? ".app__section-task-list-item-complete" : ".app__section-task-list-item";
+
+    document.querySelectorAll(seletor).forEach(elemento => elemento.remove());
+    tarefas = somenteCompletas ? tarefas.filter( tarefa => !tarefa.completa) : [];
+    atualizarTarefas();
+}
+
+
+btnRemoverConcluidas.onclick = () => removerTarefas(true);
+btbRemoverTodas.onclick = () => removerTarefas(false);
--- FRONT-END/Semana4/Praticando/LocalStorage/fokus-local-storage-main/script.js
+++ FRONT-END/Semana4/Praticando/LocalStorage/fokus-local-storage-main/script.js
@@ -11,9 +11,9 @@ const iniciarOuPausarBt = document.querySelector('#start-pause span')
 const iniciarOuPausarBtIcone = document.querySelector(".app__card-primary-butto-icon") 
 const tempoNaTela = document.querySelector('#timer')
 
-const musica = new Audio('/sons/luna-rise-part-one.mp3')
-const audioPlay = new Audio('/sons/play.wav');
-const audioPausa = new Audio('/sons/pause.mp3');
+const musica = new Audio('./sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio('./sons/play.wav');
+const audioPausa = new Audio('./sons/pause.mp3');
 const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 
 let tempoDecorridoEmSegundos = 30
@@ -79,6 +79,11 @@ const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0){
         audioTempoFinalizado.play()
         alert('Tempo finalizado!')
+        const focoAtivo = html.getAttribute('data-contexto') == 'foco';
+        if(focoAtivo){
+            const evento = new CustomEvent('FocoFinalizado')
+            document.dispatchEvent(evento);
+        }
         zerar()
         return
     }
@@ -97,13 +102,13 @@ function iniciarOuPausar() {
     audioPlay.play()
     intervaloId = setInterval(contagemRegressiva, 1000)
     iniciarOuPausarBt.textContent = "Pausar"
-    iniciarOuPausarBtIcone.setAttribute('src', `/imagens/pause.png`)
+    iniciarOuPausarBtIcone.setAttribute('src', `./imagens/pause.png`)
 }
 
 function zerar() {
     clearInterval(intervaloId) 
     iniciarOuPausarBt.textContent = "Começar"
-    iniciarOuPausarBtIcone.setAttribute('src', `/imagens/play_arrow.png`)
+    iniciarOuPausarBtIcone.setAttribute('src', `./imagens/play_arrow.png`)
     intervaloId = null
 }
 
